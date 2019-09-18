542775ec3e8caa7798da85958e55b7a028120aef
AVGasanov <avgasanov@gmail.com>
2019-09-19 02:23:28 +0400

    Add "bad" request tests to user controller
    - Add tests for OrderController

---
+++ src/test/java/com/example/demo/controllers/OrderControllerTest.java
@@ -0,0 +1,68 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.UserOrder;
+import com.example.demo.model.persistence.repositories.OrderRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class OrderControllerTest {
+
+    private OrderController orderController;
+    private UserRepository userRepository = mock(UserRepository.class);
+    private OrderRepository orderRepository = mock(OrderRepository.class);
+
+    @Before
+    public void setUp() {
+        orderController = new OrderController();
+        TestUtils.injectObjects(orderController, "userRepository", userRepository);
+        TestUtils.injectObjects(orderController, "orderRepository", orderRepository);
+    }
+
+    @Test
+    public void submitTest() throws Exception{
+        String username = "some-user";
+        User user = new User();
+        user.setId(1);
+        user.setUsername(username);
+        user.setCart(new Cart(1L, new ArrayList<>(), user, BigDecimal.TEN));
+        when(userRepository.findByUsername(username)).thenReturn(user);
+        final ResponseEntity<UserOrder> response = orderController.submit(username);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        UserOrder userOrder = response.getBody();
+        assertNotNull(userOrder);
+        assertEquals(BigDecimal.TEN, userOrder.getTotal());
+        assertEquals(new ArrayList<>(), userOrder.getItems());
+    }
+
+
+    @Test
+    public void submitTestBad() throws Exception{
+        String username = "some-user";
+        User user = new User();
+        user.setId(1);
+        user.setUsername(username);
+        user.setCart(new Cart(1L, new ArrayList<>(), user, BigDecimal.TEN));
+        when(userRepository.findByUsername(username)).thenReturn(user);
+        final ResponseEntity<UserOrder> response = orderController.submit("wrong-name");
+
+        assertNotNull(response);
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+}
